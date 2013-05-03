07ce1c58b7fe022c2c087887c6a6e0da8d0a0f2b
MichaLove <michael@sinsbeck.com>
2013-05-03 22:02:04 +0200

    Corrected missle drawing

--- launcher.lua
+++ launcher.lua
@@ -2,8 +2,8 @@ Launcher = object:New({
   radius2 = 20^2,
   firerate = 1.5, -- in seconds
   angle = 0,
-  ox = .75,
-  oy = .5,
+  ox = .5,
+  oy = 0.5,
   timeleft = 0,
   velocity = 10,
   img = love.graphics.newImage('images/launcher.png')
@@ -21,9 +21,11 @@ function Launcher:setAcceleration(dt)
     self.angle = math.atan2(dy,dx)
     
     if self.timeleft == 0 then --shoot
-			local vx = -self.velocity*math.cos(self.angle)
-			local vy = -self.velocity*math.sin(self.angle)
-			local newBullet = Missle:New({x=self.x+self.ox-Bullet.width/2,y=self.y+self.oy-Bullet.height/2,vx=vx,vy=vy})
+			local vx = -self.velocity*math.cos(self.angle)*0
+			local vy = -self.velocity*math.sin(self.angle)*0
+			local x = self.x+self.ox*self.width-0.5*15/20
+			local y = self.y+self.oy*self.height-0.5*8/20
+			local newBullet = Missle:New({x=x,y=y,vx=vx,vy=vy})
 			spriteEngine:insert(newBullet)
 			self.timeleft = self.firerate
     end
@@ -33,11 +35,11 @@ end
 function Launcher:draw()
   if self.img and self.width and self.height then
     love.graphics.draw(self.img,
-      math.floor((self.x+self.ox)*myMap.tileSize),
-      math.floor((self.y+self.oy)*myMap.tileSize),
+      math.floor((self.x+self.ox*self.width)*myMap.tileSize),
+      math.floor((self.y+self.oy*self.height)*myMap.tileSize),
       self.angle,
       1,1,
-      self.ox*myMap.tileSize,
-      self.oy*myMap.tileSize)
+      self.ox*self.width*myMap.tileSize,
+      self.oy*self.height*myMap.tileSize)
   end
 end
--- missle.lua
+++ missle.lua
@@ -1,20 +1,21 @@
 Missle = object:New({
   vx = 1,
   vy = 1,
-  maxspeed = 20,
-  seekspeed = 20,
+  maxspeed = 30,
+  seekspeed = 50,
   angle = 0,
-  ox = 0.5,
-  oy = 0,5,
+  ox = 0.75,
+  oy = 0.5,
   img = love.graphics.newImage('images/missle.png')  
 })
 
 function Missle:setAcceleration(dt)
   local dx = self.x-p.x
   local dy = self.y-p.y
+  local distance = math.sqrt(dx*dx+dy*dy)
 
-  self.vx = self.vx - self.seekspeed*dx*dt
-  self.vy = self.vy - self.seekspeed*dy*dt
+  self.vx = self.vx - self.seekspeed*dx/distance*dt
+  self.vy = self.vy - self.seekspeed*dy/distance*dt
   
   local speed = math.sqrt(self.vx^2+self.vy^2)
   
@@ -35,12 +36,12 @@ end
 function Missle:draw()
   if self.img and self.width and self.height then
     love.graphics.draw(self.img,
-      math.floor((self.x+self.ox)*myMap.tileSize),
-      math.floor((self.y+self.oy)*myMap.tileSize),
+      math.floor((self.x)*myMap.tileSize)+self.ox*self.width*myMap.tileSize,
+      math.floor((self.y)*myMap.tileSize)+self.oy*self.height*myMap.tileSize,
       self.angle,
       1,1,
-      self.ox*myMap.tileSize,
-      self.oy*myMap.tileSize)
+      self.ox*self.width*myMap.tileSize,
+      self.oy*self.height*myMap.tileSize)
   end
 end
 
