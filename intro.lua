00135582689cf8a08c20dc2eac21c9199c09d4a9
MichaLove <michael@sinsbeck.com>
2013-07-02 23:41:27 +0200

    Changed background color

--- intro.lua
+++ intro.lua
@@ -5,7 +5,8 @@ function intro:draw ()
 	self.height = self.height or love.graphics.getHeight()
 	self.width = self.width or love.graphics.getWidth()
   local done = timer/self.duration;
-  love.graphics.setBackgroundColor(20,120,170)        
+  --love.graphics.setBackgroundColor(20,120,170)        
+  love.graphics.setBackgroundColor(80,150,205)  
   game.draw()
   love.graphics.setColor(0,0,0,255*(1-done))
   love.graphics.rectangle('fill',0,0,self.width,self.height)
@@ -19,6 +20,7 @@ function intro:update(dt)
   if timer > self.duration then
     mode = 'game'
     timer = 0
-    love.graphics.setBackgroundColor(20,120,170)        
+    --love.graphics.setBackgroundColor(20,120,170)        
+    love.graphics.setBackgroundColor(80,150,205)   
   end
 end
