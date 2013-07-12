f2ba6e9799ed05fbf4e008dbaa421051eff122ae
Germanunkol <Germanunkol@yahoo.com>
2013-07-12 19:41:15 +0200

    Added example of level-end screen (very rough)

--- main.lua
+++ main.lua
@@ -1,108 +1,69 @@
-require 'utility'
-require 'camera'
-require 'game'
-require 'spritefactory'
-require 'menu'
-require 'map'
-require 'intro'
-require 'campaign'
+-- global list that holds the death animations
+-- (only single frame images for now):
+deathAnimations = {}
+deathAnimations[1] = love.graphics.newImage("images/death1.png")
+deathAnimations[2] = love.graphics.newImage("images/death2.png")
+deathAnimations[3] = love.graphics.newImage("images/death3.png")
 
+-- list of deaths... in a real-world example, the game would save
+-- every death at the end of this list
+deaths = {}
+deathTimers = {}
+
+local numDeaths = math.random(4,8)
 
 function love.load()
 
-	-- hide mouse
-	love.mouse.setVisible(false)
-
-	Camera:init()
-
-	recorder = false
-	screenshots = {}
-	recorderTimer = 0
-
-  mode = 'game'
-  timer = 0
-    
-  Campaign:reset()
-  
-  initAll()
-  -- Creating Player
-  p = spriteFactory('player')
-  --p = Player:New()
-  --spriteEngine:insert(p)
-  
-  gravity = 22  
-  myMap:start(p)
-
-  --love.graphics.toggleFullscreen()
-end
-
-function love.update(dt)
-  if mode == 'game' then
-    game:update(dt)
-  elseif mode == 'menu' then
-    menu:update(dt)
-  elseif mode == 'intro' then
-    intro:update(dt)
-  end
 end
 
 function love.draw()
-  --love.graphics.scale(0.75,0.75)
-  if mode == 'game' then
-    game:draw()
-  elseif mode == 'menu' then
-    menu:draw()
-  elseif mode == 'intro' then
-    intro:draw()
-  end
-end
-
-function love.keypressed(key)
-  if key == 't' then
-    love.graphics.newScreenshot():encode('screenshot.png')
-    print('Saved screenshot')
-  end
-  if key == 'escape' then
-    love.event.quit()
-  end
-  
-  if key == 'p' then
-    myMap:start(p)
-  end
-  
-  if key == 'o' then
-    Campaign:reset()
-    myMap:start(p)
-  end
-  
-  if mode == 'game' then
-    game.keypressed(key)
-  elseif mode == 'menu' then
-    menu.keypressed(key)
-  end
-end
-
-function love.keyreleased(key)  
-  if mode == 'game' then
-    game.keyreleased(key)
-  elseif mode == 'menu' then
-    menu.keyreleased(key)
-  end
-end
-
-function love.joystickpressed(joystick, button)
-	if mode == 'game' then
-			game.joystickpressed(joystick, button)
-		else
-			menu.joystickpressed(joystick, button)
+	w = 10
+	
+	love.graphics.setColor(128, 179, 255)
+	--love.graphics.setColor(28, 79, 155)
+	love.graphics.rectangle("fill",0, love.graphics.getHeight()/2-60, love.graphics.getWidth(), 60)
+	love.graphics.setColor(255,255,255)
+	for i=1,#deaths do
+		h = - math.min(deathTimers[i], 1)*deathAnimations[1]:getHeight() + love.graphics.getHeight()/2 --deathAnimations[1]:getHeight()
+		love.graphics.draw(deathAnimations[deaths[i]], w, h)
+		w = w + deathAnimations[deaths[i]]:getWidth() + 25
 	end
-	if button == 9 then Campaign:reset() myMap:start(p) end
+
+	love.graphics.setColor(172,157,147)
+	love.graphics.setColor(0,0,0)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2, love.graphics.getWidth(), 50)
+	--[[love.graphics.setColor(132,127,117)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2+50, love.graphics.getWidth(), 5)
+	love.graphics.setColor(102,97,87)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2+55, love.graphics.getWidth(), 5)
+	love.graphics.setColor(50,30,25)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2+60, love.graphics.getWidth(), 5)
+	love.graphics.setColor(20,10,10)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2+65, love.graphics.getWidth(), 5)
+	love.graphics.setColor(0,0,0)
+love.graphics.rectangle("fill",0, love.graphics.getHeight()/2, love.graphics.getWidth(), 2)
+]]--
+	love.graphics.setColor(255,255,255)
+
+	
 end
 
-function love.joystickreleased(joystick, button)
-if mode == 'game' then
-    game.joystickreleased(joystick, button)
-  else
-    menu.joystickreleased(joystick, button)
-  end
+timeUntilNewImg = 1
+timeRunning = 1
+function love.update(dt)
+	
+	for i = 1,#deathTimers do
+		deathTimers[i] = deathTimers[i] + dt*deathTimers[i] + dt*timeRunning
+	end
+	timeRunning = timeRunning + dt
+	timeUntilNewImg = timeUntilNewImg - dt*2
+	if timeUntilNewImg < 0 and numDeaths > #deaths then
+		deaths[#deaths+1] = math.random(3)	-- add a death to list
+		deathTimers[#deaths] = 0
+		if #deaths > 2 then
+			timeUntilNewImg = .5
+		else
+			timeUntilNewImg = 2
+		end
+	end
 end
