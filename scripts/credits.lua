ba24a67f69bf8cf59051555f92271dc3520a1429
Germanunkol <Germanunkol@yahoo.com>
2013-08-24 14:32:56 +0200

    Added images for credits

---
+++ scripts/credits.lua
@@ -0,0 +1,15 @@
+
+---------------------------------------------------------
+-- Display credits when it is selected from the menu.
+---------------------------------------------------------
+
+local credits = {}
+
+function credtis.update( dt )
+
+
+end
+
+
+
+return credits
