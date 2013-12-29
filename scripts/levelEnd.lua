8757eec15d2449b09b987ff0dba04d335adca922
MichaLove <michael@sinsbeck.com>
2013-12-29 13:07:44 +0100

    Removed random debugging statistics

--- scripts/levelEnd.lua
+++ scripts/levelEnd.lua
@@ -144,12 +144,12 @@ function levelEnd:display( )	-- called when level is won:
 
 	--statList["death_fall"] = math.random(10)
 	--statList["death_spikey"] = math.random(10)
-	for k,v in pairs( statList ) do
+	--[[for k,v in pairs( statList ) do
 		statList[k] = 0
 	end
 	statList["death_walker"] = math.random(5)
 	statList["death_spikey"] = math.random(5)
-	statList["death_fall"] = math.random(5)
+	statList["death_fall"] = math.random(5)]]
 
 	-- create a list which holds all the values which were relevant for this
 	-- level (i.e. their values are not zero - the event happened)
