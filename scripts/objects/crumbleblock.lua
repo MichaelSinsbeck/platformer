6f5b5befd7567f451fa0e79e87228dbe243df053
MichaLove <michael@sinsbeck.com>
2014-02-15 14:28:07 +0100

    New concept for crumbleblock visualization

--- scripts/objects/crumbleblock.lua
+++ scripts/objects/crumbleblock.lua
@@ -1,12 +1,25 @@
 Crumbleblock = object:New({
 	tag = 'crumbleblock',
-  marginx = 0.8,
-  marginy = 0.8,
+  --marginx = 0.8,
+  --marginy = 0.8,
+  marginx = 2,
+  marginy = 2,
   state = 'sleep',
   solid = true,
-  spreadSpeed = 8,  -- For explosion
+  spreadSpeed = 2,  -- For explosion
   particleRotSpeed = 5, -- For explosion
-  vis = {Visualizer:New('crumbleblock')},
+  deathtime = 0.1, -- time from frist crumbling till end
+  nCrumbs = 9,
+  --vis = {Visualizer:New('crumbleblock')},
+  vis = {Visualizer:New('crumble1',{relY = -.25, relX = -.25}),
+				 Visualizer:New('crumble2',{relY = 0,    relX = -.25}),
+				 Visualizer:New('crumble3',{relY = .25, relX = -.25}),
+				 Visualizer:New('crumble4',{relY = -.25, relX = 0}),
+				 Visualizer:New('crumble5',{relY = 0,    relX = 0}),
+				 Visualizer:New('crumble6',{relY = .25, relX = 0}),
+				 Visualizer:New('crumble7',{relY = -.25, relX = .25}),
+				 Visualizer:New('crumble8',{relY = 0,    relX = .25}),
+				 Visualizer:New('crumble9',{relY = .25, relX = .25}),	},
 	properties = {
 		lifetime = newProperty({.5 , 1, 1.5, 2, 2.5, 3},nil,2)
 	},  
@@ -22,7 +35,20 @@ function Crumbleblock:postStep(dt)
 		self.state = 'wait'
 		self.vis[1].timer = 0
 	elseif self.state == 'wait' then
-		self.vis[1].alpha = 120+125*math.max(1-self.vis[1].timer/self.lifetime,0)
+		for i = #self.vis,1,-1 do
+			local vis = self.vis[i]
+			if (self.lifetime - self.vis[1].timer)/self.deathtime < i / (self.nCrumbs-1) then
+			
+				local x, y = self.x + vis.relX, self.y + vis.relY
+				local vx = (math.random()*2-1)*self.spreadSpeed
+				local rotSpeed = self.particleRotSpeed * (math.random()*2-1)
+				local newParticle = Particle:New({x=x,y=y,vx = vx,vy = 0,rotSpeed = rotSpeed,vis = {Visualizer:New(vis.animation)} })
+				spriteEngine:insert(newParticle)
+			  self.vis[i] = nil
+			end
+		end
+		--self.vis[1].alpha = 120+125*math.max(1-self.vis[1].timer/self.lifetime,0)
+		--[[
 		if self.vis[1].timer > self.lifetime then
 			myMap.collision[math.floor(self.x)][math.floor(self.y)] = nil
 			myMap:queueShadowUpdate()
@@ -31,14 +57,16 @@ function Crumbleblock:postStep(dt)
 				local angle, magnitude = math.pi*2*math.random(), 0.7+math.random()*0.3
 				
 				local vx = math.cos(angle)*self.spreadSpeed*magnitude
-				local vy = (math.sin(angle)-0.2)*self.spreadSpeed*magnitude
+				local vy = (math.sin(angle)-0)*self.spreadSpeed*magnitude
+				--local vx = 0
+				--local vy = 0
 				local x,y = self.x + math.random()-0.5, self.y+math.random()-0.5
 				
 				local rotSpeed = self.particleRotSpeed * (math.random()*2-1)
-				local animation = 'crumble' .. math.random(1,4)
-				local newParticle = Particle:New({x=self.x,y=self.y,vx = vx,vy = vy,rotSpeed = rotSpeed,vis = {Visualizer:New(animation)} })
+				local animation = 'crumble' .. math.random(1,12)
+				local newParticle = Particle:New({x=x,y=y,vx = vx,vy = vy,rotSpeed = rotSpeed,vis = {Visualizer:New(animation)} })
 				spriteEngine:insert(newParticle)
 			end
-		end
+		end--]]
 	end
 end
