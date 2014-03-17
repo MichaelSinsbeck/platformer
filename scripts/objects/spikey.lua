6119aa2fb7ca496266c646c29035b340f8c5f3ed
Germanunkol <Germanunkol@yahoo.com>
2014-03-17 21:41:19 +0100

    Fixed bug: Meat once again spawns correctly without crashing game.

--- scripts/objects/spikey.lua
+++ scripts/objects/spikey.lua
@@ -8,7 +8,7 @@ function Spikey:setAcceleration(dt)
 	if not p.dead and self:touchPlayer() then
     p.dead = true
     levelEnd:addDeath("death_spikey")
-    Meat:spawn(p.oldx,p.oldy,self.vx,self.vy,12)
+    objectClasses.Meat:spawn(p.oldx,p.oldy,self.vx,self.vy,12)
   end  
 end
 
