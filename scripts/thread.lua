7574bc85853081faa889068bdceb5a68022e2364
Germanunkol <Germanunkol@yahoo.com>
2014-05-25 18:23:58 +0200

    Added fallback error message when thread fails. This is important because otherwise the main thread might wait forever for an error message if we make a mistake programming the background threads. In all 'normal' cases, this default error message should never be displayed.

--- scripts/thread.lua
+++ scripts/thread.lua
@@ -4,6 +4,8 @@
 -- For now, all scripts that should be executed in backgroun threads are (blocking) html requests.
 -- The printChannel is for printing messages to the standard console, the statusChannel is used for status updates. Once "success" or "failed" is send thorugh the statusChannel, the game considers this thread to be done running.
 -- Once the script has finished, this thread sends back info to the main thread and terminates.
+--
+-- IMPORTANT! Any scripts called by this should return 'false' and an error message upon failure, which will then be passed back to the main thread. The main thread expects such a failure message, if it's not supplied, it will wait forever.
 require("scripts/threadUtils")
 
 local args = {...}
@@ -15,17 +17,18 @@ functionName = args[4]
 
 -- Load the given script.
 dofile(scriptFile)
---for k, v in pairs(_G) do
---	print("\t",k, v)
---end
 
 -- Execute the function. Since it has been loaded and is global, it should now be in _G:
 local result, msg = _G[functionName]( args[5], args[6], args[7], args[8], args[9] )
+
 
 if result then
 	statusChannel:push("success")
 else
 	statusChannel:push("failed")
-	statusChannel:push( msg )		-- supply reason for failing to parent threat.
+	if not msg then
+		msg = "Unknown error"	-- msg is needed, so in case there is none, use this default error.
+	end
+	statusChannel:push( msg )	-- supply reason for failing to parent threat.
 end
 
