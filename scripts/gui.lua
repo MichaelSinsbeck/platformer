1fed683ec0fc4f187197c0eb8535145ad1ed3d78
MichaLove <michael@sinsbeck.com>
2014-06-29 13:23:40 +0200

    Fixed color issue with gui

--- scripts/gui.lua
+++ scripts/gui.lua
@@ -14,7 +14,7 @@ function gui.draw()
 	
 	love.graphics.setColor(255,255,255,50)
 	love.graphics.rectangle('fill',0,0,10*s*(p.maxJumps-1),10*s)
-	
+	love.graphics.setColor(255,255,255)
 	
 	for i = 1,p.jumpsLeft do
 		FullViz:draw((10*i-5)*s,5*s)
@@ -23,8 +23,7 @@ function gui.draw()
 	for i = p.jumpsLeft+1,p.maxJumps-1 do
 		EmptyViz:draw((10*i-5)*s,5*s)
 	end
+	
 end
-  --jumpsLeft = 0,
-  --maxJumps = 1,
 
 return gui
