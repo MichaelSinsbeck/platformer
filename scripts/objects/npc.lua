7fe3ea58e75b67f79af0695553cfcad42d6e44e7
MichaLove <michael@sinsbeck.com>
2014-08-03 19:01:50 +0200

    Added npc-object: Display some text when player is close

---
+++ scripts/objects/npc.lua
@@ -0,0 +1,37 @@
+local t1 = 'A proper ninja must jump high. Wear the white bandana and learn the art of jumping.'
+local t2 = 'A proper ninja masters the wall jump. Hug the wall and jump again.'
+local t3 = 'This is the third text'
+local t4 = 'This is the forth text'
+local t5 = 'This is the fifth text'
+
+
+local Npc = object:New({
+	tag = 'Npc',
+  marginx = .8,
+  marginy = .8,
+  isInEditor = true,
+  radius = 4,
+  vis = {
+		Visualizer:New('placeholder08'),
+		Visualizer:New( nil, {active = false,relY = -2}, 'hallo' ),
+  },
+	properties = {
+		text = utility.newCycleProperty({t1,t2,t3,t4,t5}, {1,2,3,4,5}),
+	}, 
+})
+
+function Npc:applyOptions()
+	self.vis[2].text = self.text
+	self.vis[2].ox = 0.5*fontSmall:getWidth(self.text)/Camera.scale
+end
+
+function Npc:setAcceleration(dt)
+end
+
+function Npc:postStep(dt)
+	local dx = p.x - self.x
+	local dy = p.y - self.y
+	self.vis[2].active = (utility.pyth(dx,dy) < self.radius)
+end
+
+return Npc
