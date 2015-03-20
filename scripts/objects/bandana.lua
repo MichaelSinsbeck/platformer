67b694a1c1585c1930cfeb008657184845810fa6
MichaLove <michael@sinsbeck.com>
2015-03-20 07:58:13 +0100

    Changed bandana power-up item drawing code

--- scripts/objects/bandana.lua
+++ scripts/objects/bandana.lua
@@ -14,7 +14,7 @@ local Bandana = object:New({
 })
 
 function Bandana:applyOptions()
-	self:setAnim(self.color .. 'Bandana',true,2)
+	--self:setAnim(self.color .. 'Bandana',true,2)
 end
 
 function Bandana:setAcceleration(dt)
@@ -35,6 +35,20 @@ function Bandana:setAcceleration(dt)
 		--p.bandana = self.color
 		self:kill()
   end
+end
+
+function Bandana:draw()
+	local x = self.x*8*Camera.scale
+	local y = self.y*8*Camera.scale
+	
+	self.vis[1]:draw(x,y,true)
+	local color = utility.bandana2color[self.color]
+	if color and not self.anchor then
+		local r,g,b = love.graphics.getColor()
+		love.graphics.setColor(color[1],color[2],color[3],255)
+		self.vis[2]:draw(x,y,true)
+		love.graphics.setColor(r,g,b)
+	end
 end
 
 return Bandana
