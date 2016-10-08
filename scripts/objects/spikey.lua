70c9b35ddb8e93733e68323eb47fc0d30ad314a6
Michael Sinsbeck <michael@sinsbeck.com>
2016-10-08 20:48:29 +0200

    Resurrected spikey: We need this object

---
+++ scripts/objects/spikey.lua
@@ -0,0 +1,16 @@
+local Spikey = object:New({
+	tag = 'Spikey',
+  semiheight = 0.5,
+  semiwidth = 0.5,
+})
+
+function Spikey:setAcceleration(dt)
+	if not p.dead and self:touchPlayer() then
+		p:kill()
+    self:playSound('death')
+    levelEnd:addDeath("death_spikey")
+    objectClasses.Meat:spawn(p.oldx,p.oldy,self.vx,self.vy,12)
+  end  
+end
+
+return Spikey
