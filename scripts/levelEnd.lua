84c395796840d87e44772b9c954392b45b11f5f1
Germanunkol <Germanunkol@yahoo.com>
2016-10-09 18:27:28 +0200

    Moved keypresses computer.

--- scripts/levelEnd.lua
+++ scripts/levelEnd.lua
@@ -182,7 +182,7 @@ function levelEnd:display( )	-- called when level is won:
 		k = math.random(#relevantList)
 		if i == 1 then
 			for j = 1,#relevantList do
-				if relevantList[j].statType == "death_imitator" then
+				if relevantList[j].statType == "keypresses" then
 					k = j
 					break
 				end
